fc8a53edb1ee78d5d8c028216e6a8ed7d301a792
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-21 15:26:40 +0000

    add extension to bashrc and add script to setup elixir_ls.

--- scripts/bash_setup.sh
+++ scripts/bash_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LINE=". $HOME/.bashrc_ext"
+LINE='. "$HOME/.bashrc_ext"'
 FILE="$HOME/.bashrc"
 grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"  
 unset -v LINE FILE
---
+++ scripts/elixir_ls_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOT_DIR_DOWNLOADS=$HOME/downloads
+DOT_DIR_BIN=$HOME/bin
+DOT_DIR_APPS=$HOME/apps
+
+_elixir_ls_version=v0.22.1
+
+cd $DOT_DIR_DOWNLOADS
+curl -fLO https://github.com/elixir-lsp/elixir-ls/releases/download/${_elixir_ls_version}/elixir-ls-${_elixir_ls_version}.zip
+unzip -u elixir-ls-${_elixir_ls_version}.zip -d ${DOT_DIR_APPS}/elixir-ls
+chmod +x ${DOT_DIR_APPS}/elixir-ls/language_server.sh
+ln -s ${DOT_DIR_APPS}/elixir-ls/language_server.sh ${DOT_DIR_BIN}/elixir_ls
+
+unset -v _elixir_ls_version
+
