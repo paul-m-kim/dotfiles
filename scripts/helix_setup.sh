007473bbdabf7d57c470d99c64cf3b6b9fede270
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-21 23:10:39 +0000

    use the new apps directory when installing helix.

--- scripts/helix_setup.sh
+++ scripts/helix_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-HELIX_VERSION='24.07'
 
-wget -P $DOT_DIR_DOWNLOADS "https://github.com/helix-editor/helix/releases/download/$HELIX_VERSION/helix-$HELIX_VERSION-x86_64-linux.tar.xz" 
-tar -xvf $DOT_DIR_DOWNLOADS/helix-$HELIX_VERSION-x86_64-linux.tar.xz --directory $DOT_DIR_DOWNLOADS/
+DOT_DIR_DOWNLOADS=$HOME/downloads
+DOT_DIR_BIN=$HOME/bin
+DOT_DIR_APPS=$HOME/apps
 
-cp $DOT_DIR_DOWNLOADS/helix-$HELIX_VERSION-x86_64-linux/hx $DOT_DIR_BIN/hx
-cp -r $DOT_DIR_DOWNLOADS/helix-$HELIX_VERSION-x86_64-linux/runtime/ $HOME/.config/helix/ 
+_helix_version='24.07'
+
+wget -P $DOT_DIR_DOWNLOADS "https://github.com/helix-editor/helix/releases/download/${_helix_version}/helix-${_helix_version}-x86_64-linux.tar.xz" 
+tar -xvf $DOT_DIR_DOWNLOADS/helix-${_helix_version}-x86_64-linux.tar.xz --directory $DOT_DIR_APPS/
+
+cp $DOT_DIR_APPS/helix-${_helix_version}-x86_64-linux/hx $DOT_DIR_BIN/hx
+cp -r $DOT_DIR_APPS/helix-${_helix_version}-x86_64-linux/runtime/ $HOME/.config/helix/ 
 
