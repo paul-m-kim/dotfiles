18dfd9d1360bb1846203db2c3e7905940819d58e
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-22 09:09:46 -0700

    helix looks for elixir-ls not elixir_ls

--- scripts/elixir_ls_setup.sh
+++ scripts/elixir_ls_setup.sh
@@ -10,7 +10,7 @@ cd $DOT_DIR_DOWNLOADS
 curl -fLO https://github.com/elixir-lsp/elixir-ls/releases/download/${_elixir_ls_version}/elixir-ls-${_elixir_ls_version}.zip
 unzip -u elixir-ls-${_elixir_ls_version}.zip -d ${DOT_DIR_APPS}/elixir-ls
 chmod +x ${DOT_DIR_APPS}/elixir-ls/language_server.sh
-ln -s ${DOT_DIR_APPS}/elixir-ls/language_server.sh ${DOT_DIR_BIN}/elixir_ls
+ln -s ${DOT_DIR_APPS}/elixir-ls/language_server.sh ${DOT_DIR_BIN}/elixir-ls
 
 unset -v _elixir_ls_version
 
