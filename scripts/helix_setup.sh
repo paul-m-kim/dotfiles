b666556a6084a536f141985528272309e00d6dbd
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-23 02:15:13 +0000

    update helix config and setup.

--- scripts/helix_setup.sh
+++ scripts/helix_setup.sh
@@ -9,6 +9,10 @@ _helix_version='24.07'
 wget -P $DOT_DIR_DOWNLOADS "https://github.com/helix-editor/helix/releases/download/${_helix_version}/helix-${_helix_version}-x86_64-linux.tar.xz" 
 tar -xvf $DOT_DIR_DOWNLOADS/helix-${_helix_version}-x86_64-linux.tar.xz --directory $DOT_DIR_APPS/
 
-cp $DOT_DIR_APPS/helix-${_helix_version}-x86_64-linux/hx $DOT_DIR_BIN/hx
-cp -r $DOT_DIR_APPS/helix-${_helix_version}-x86_64-linux/runtime/ $HOME/.config/helix/ 
+if [ ! -d "$HOME/.config/helix" ]; then
+  mkdir -p "$HOME/.config/helix"
+fi
+
+ln -s $DOT_DIR_APPS/helix-${_helix_version}-x86_64-linux/runtime/ $HOME/.config/helix/runtime 
+ln -s $DOT_DIR_APPS/helix-${_helix_version}-x86_64-linux/hx $DOT_DIR_BIN/hx
 
