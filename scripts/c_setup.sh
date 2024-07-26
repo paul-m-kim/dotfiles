894465a86fb98474094c4c5f1e9f64491a1500ed
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-26 07:07:05 +0000

    add note for C lsp.

---
+++ scripts/c_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# clangd relies on a JSON compilation database https://clang.llvm.org/docs/JSONCompilationDatabase.html
+# specified as compile_commands.json or, for simpler projects, a compile_flags.txt. 
+# For details on how to automatically generate one using CMake look here. https://cmake.org/cmake/help/latest/variable/CMAKE_EXPORT_COMPILE_COMMANDS.html
+# Alternatively, you can use Bear. https://github.com/rizsotto/Bear
+
