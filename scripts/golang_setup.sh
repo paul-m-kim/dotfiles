4cbcf3628441ff07fb144e0ee7d8169330a91d58
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-26 16:00:15 +0000

    package name and expected executable is different.

--- scripts/golang_setup.sh
+++ scripts/golang_setup.sh
@@ -4,10 +4,17 @@ DOT_DIR_DOWNLOADS=$HOME/downloads
 DOT_DIR_BIN=$HOME/bin
 DOT_DIR_APPS=$HOME/apps
 GOLANG_VERSION=1.22.5
-declare -a GOLANG_PACKAGES=('gopls'
-                            'dlv'
-                            'golangci-lint-langserver'
-                            'goimports')
+
+# declare -a GOLANG_PACKAGES=('gopls'
+#                             'dlv'
+#                             'golangci-lint-langserver'
+#                             'goimports')
+
+declare -A GOLANG_PACKAGES                           
+GOLANG_PACKAGES['gopls']='gopls'
+GOLANG_PACKAGES['dlv']='dlv'
+GOLANG_PACKAGES['golangci-lint-langserver']='golangci-lint'
+GOLANG_PACKAGES['goimports']='goimports'
 
 wget -nc -P ${DOT_DIR_DOWNLOADS} https://go.dev/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz
 rm -rf ${DOT_DIR_APPS}/go
@@ -29,8 +36,12 @@ go install golang.org/x/tools/cmd/goimports@latest
 # Linter
 go install github.com/nametake/golangci-lint-langserver@latest     
 
-for PACKAGE in "${GOLANG_PACKAGES[@]}"
-do
-  ln -s $GOPATH/bin/${PACKAGE} ${DOT_DIR_BIN}/${PACKAGE}
-done
+# for PACKAGE in "${GOLANG_PACKAGES[@]}"
+# do
+#   ln -s $GOPATH/bin/${PACKAGE} ${DOT_DIR_BIN}/${PACKAGE}
+# done
 
+for PACKAGE in "${!GOLANG_PACKAGES[@]}"
+do
+  ln -s $GOPATH/bin/${PACKAGE} ${DOT_DIR_BIN}/${GOLANG_PACKAGES[$PACKAGE]}
+done
