545d38cc80246c9f8722ac1e123578b8d1686333
Paul Kim <muhyun.paul.kim@gmail.com>
2024-07-26 08:25:47 +0000

    rust-analyzer bugfix.

--- scripts/rust_analyzer_setup.sh
+++ scripts/rust_analyzer_setup.sh
@@ -8,7 +8,7 @@ RUST_ANALYZER_VERSION='2024-07-22'
 
 # wget -nc -P ${DOT_DIR_DOWNLOADS} https://github.com/rust-lang/rust-analyzer/releases/download/${RUST_ANALYZER_VERSION}/rust-analyzer-x86_64-unknown-linux-musl.gz
 wget -nc -P ${DOT_DIR_DOWNLOADS} https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz
-gunzip -c ${DOT_DIR_DOWNLOADS}/rust-analyzer-x86_64-unknown-linux-musl.gz > ${DOT_DIR_APPS}/rust-analyzer
+gunzip -c ${DOT_DIR_DOWNLOADS}/rust-analyzer-x86_64-unknown-linux-gnu.gz > ${DOT_DIR_APPS}/rust-analyzer
 chmod +x ${DOT_DIR_APPS}/rust-analyzer
 ln -s ${DOT_DIR_APPS}/rust-analyzer ${DOT_DIR_BIN}/rust-analyzer
 
