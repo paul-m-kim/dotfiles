1ff9a63bf5369085e62e8bbd0450ebcd8d1c8dba
Paul Kim <muhyun.paul.kim@gmail.com>
2024-09-24 17:06:25 -0700

    fix problems with asdf setup script.

--- scripts/asdf_setup.sh
+++ scripts/asdf_setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-DIR_SCRIPTS=$(dirname $0)
-FILE='$HOME/.bashrc'
+# environment variables
+DIR_SCRIPTS="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+. ${DIR_SCRIPTS}/../bash/.bashrc_ext
+
+FILE=${HOME}/.bashrc
 declare -a LINES_IN_BASHRC=('. "$HOME/.asdf/asdf.sh"'
                             '. "$HOME/.asdf/completions/asdf.bash"')
 
@@ -11,15 +14,20 @@ do
   grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"  
 done
 
-erlang_build="27.0.1"
-elixir_buihd="1.17.2-otp-27"
+source ${HOME}/.bashrc
 
-source $DIR_SCRIPTS/erlang_deps.sh
+sudo apt install curl
+
+erlang_build="27.1"
+elixir_build="1.17.3-otp-27"
+
+source ${DIR_SCRIPTS}/erlang_deps.sh
 asdf plugin add erlang
-export KERL_BUILD_DOCS=yes && asdf install erlang $erlang_build
-asdf global erlang $erlang_build
+export KERL_BUILD_DOCS=yes && asdf install erlang ${erlang_build}
+asdf global erlang ${erlang_build}
 
+source ${DIR_SCRIPTS}/elixir_deps.sh
 asdf plugin add elixir
-asdf install elixir $elixir_build
-asdf global elixir $elixir_build
+asdf install elixir ${elixir_build}
+asdf global elixir ${elixir_build}
 
