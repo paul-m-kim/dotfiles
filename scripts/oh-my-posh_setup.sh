2b4807fda72c41fcdd3623aaa22d69f875adc794
Paul Kim <muhyun.paul.kim@gmail.com>
2025-01-28 15:13:53 -0800

    fix oh-my-posh execution at start.

--- scripts/oh-my-posh_setup.sh
+++ scripts/oh-my-posh_setup.sh
@@ -6,7 +6,8 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
-LINE='eval "$(oh-my-posh init bash)"'
+# by default, .bashrc is run before ~/bin is added to path
+LINE='eval "$(~/bin/oh-my-posh init bash)"'
 FILE="$HOME/.bashrc_ext"
 grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"  
 unset -v LINE FILE
