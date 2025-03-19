2ed684dcf1834c24f226d948280b09378184f57c
Paul Kim <muhyun.paul.kim@gmail.com>
2025-03-19 07:03:26 +0000

    narrow down the boilerplate to just argument parsing.

--- scripts/boilerplate.sh
+++ scripts/boilerplate.sh
@@ -6,22 +6,18 @@ print_help() {
   local path_this_script=${1}
 
   # https://en.wikipedia.org/wiki/Usage_message
-  echo "================"
-  echo "- dir_downloads "
-  echo "- dir_apps      "
-  echo "- dir_bins      "
-  echo "-               "
-  echo "================"
+  echo "==================================================================================="
+  echo "[help] ${path_this_script} -abcd [-e | -f] --golf [-h | --hotel] val_hotel
+                                   <arg_pos_1> <arg_pos_2> [[[arg_opt_1] arg_opt_2] ...]"
+  echo "==================================================================================="
 
 }
 
 # constants
-NUM_POS_ARGS=3
-NUM_OPT_ARGS=1
+NUM_POS_ARGS=0
+NUM_OPT_ARGS=0
 NUM_OPT_FLAGS=0
-
-declare -A PACKAGES_APT=(["echo"]="echo")
-declare -A PACKAGES_PIP=(["black"]="black")
+NUM_EXT_ARGS_MAX=10
 
 # runtime
 path_this_script=${0}
@@ -39,103 +35,42 @@ if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
 fi
 
 # check number of arguments
-if (($# < NUM_POS_ARGS)) || (($# > (NUM_POS_ARGS + (NUM_OPT_ARGS * 2) + NUM_OPT_FLAGS))); then
+if (($# < NUM_POS_ARGS)) || (($# > (NUM_POS_ARGS + (NUM_OPT_ARGS * 2) + NUM_OPT_FLAGS + NUM_EXT_ARGS_MAX))); then
   echo "[error] invalid number of arguments"
   print_help "${path_this_script}"
   exit 1
 fi
 
-# positional
+# args optional - defaults
 
-# relative path to absolute path
-# path_abs=$(readlink --canonicalize ${path})
-# path_abs=$(cd ${path}; pwd)
-dir_downloads=$(readlink --canonicalize "${1}")
-dir_apps=$(readlink --canonicalize "${2}")
-dir_bin=$(readlink --canonicalize "${3}")
-shift ${NUM_POS_ARGS}
-
-# optional - defaults
-download_pkgs=false
-
-# optional
+# args optional labelled
 while (($# > 0)); do
   case $1 in
-    -p | --download-pkgs)
-      download_pkgs=true
-      shift 1
-      ;;
-    *)
+    -*)
       echo "[error] ${1} is an invalid option"
       print_help "${path_this_script}"
       exit
       ;;
+    *)
+      break
+      ;;
   esac
 done
 
-# create directories
-if [ ! -d "${dir_downloads}" ]; then
+# args positional
+shift ${NUM_POS_ARGS}
+
+# args optional and extra
+if (($# > NUM_EXT_ARGS_MAX)); then
+  echo "[error] there are too many extra arguments"
   exit 1
-  # mkdir -p "${dir_downloads}"
-fi
-
-if [ ! -d "${dir_apps}" ]; then
-  exit 1
-  # mkdir -p "${dir_apps}"
-fi
-
-if [ ! -d "${dir_bin}" ]; then
-  exit 1
-  # mkdir -p "${dir_bin}"
-fi
-
-if [ ! -d "${dir_bin}" ]; then
-  exit 1
-  # mkdir -p "${dir_bin}"
-fi
-
-# install apt packages
-for cmd in "${!PACKAGES_APT[@]}"; do
-  if [ "${cmd}" != '' ] && [ ! "$(command -v "${cmd}")" ] >/dev/null 2>&1; then
-    if ${download_pkgs}; then
-      echo "[info] apt package ${PACKAGES_APT["${cmd}"]} not found"
-      if [ "$EUID" -ne 0 ]; then
-        echo "[error] run this script in sudo to install missing apt packages"
-        exit 1
-      else
-        echo "[info] downloading ${PACKAGES_APT["${cmd}"]}"
-        sudo apt install "${PACKAGES_APT["${cmd}"]}" -y
-      fi
-    else
-      echo "[error] apt package ${PACKAGES_APT["${cmd}"]} not found"
-      exit 1
-    fi
-  fi
-done
-
-# install pip packages
-dir_python="${dir_apps}/python"
-
-if [ ! -d "${dir_python}" ]; then
-  mkdir -p "${dir_python}"
 else
-  python3 -m venv "${dir_python}"
-  . "${dir_python}/bin/activate"
+  declare -a args_extra=()
+  while (($# > 0)); do
+    args_extra+=("${1}")
+    shift 1
+  done
 fi
-
-for cmd in "${!PACKAGES_PIP[@]}"; do
-  if [ "${cmd}" != '' ] && [ ! "$(command -v "${cmd}")" ] >/dev/null 2>&1; then
-    if ${download_pkgs}; then
-      echo "[info] pip package ${PACKAGES_PIP["${cmd}"]} not found"
-      echo "[info] downloading ${PACKAGES_PIP["${cmd}"]}"
-      pip3 install "${PACKAGES_PIP["${cmd}"]}"
-      ln -s "${dir_python}"/bin/"${cmd}" "${dir_bin}"/"${cmd}"
-    else
-      echo "[error] pip package ${PACKAGES_APT["${cmd}"]} not found"
-      exit 1
-    fi
-  fi
-done
 
 # business
 echo "[info] success"
