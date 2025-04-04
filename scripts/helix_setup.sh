62e774f3e6a46c227aaa535f518bfa9438b5592f
Paul Kim <muhyun.paul.kim@gmail.com>
2025-04-04 05:21:47 +0000

    update helix setup script.

--- scripts/helix_setup.sh
+++ scripts/helix_setup.sh
@@ -6,28 +6,29 @@ print_help() {
   local path_this_script=${1}
 
   # https://en.wikipedia.org/wiki/Usage_message
-  echo "================"
-  echo "- dir_downloads "
-  echo "- dir_apps      "
-  echo "- dir_bins      "
-  echo "-               "
-  echo "================"
+  echo "==================================================================================="
+  echo "[help] ${path_this_script} [-p | --download-pkgs]
+                                   <dir_downloads> <dir_apps> <dir_bin>"
+  echo ""
+  echo "       -p, --download_pkgs            download and install any missing pkgs"
+  echo "==================================================================================="
 
 }
 
 # constants
 NUM_POS_ARGS=3
-NUM_OPT_ARGS=1
-NUM_OPT_FLAGS=0
-
-declare -A DEPENDENCIES=(["echo"]="echo")
+NUM_OPT_ARGS=0
+NUM_OPT_FLAGS=1
+NUM_EXT_ARGS_MAX=0
 
 # runtime
 path_this_script=${0}
-dir_this_script="$(
-  cd -- "$(dirname "${path_this_script}")" >/dev/null 2>&1
-  pwd -P
-)"
+if [ -f "${path_this_script}" ]; then
+  dir_this_script="$(
+    cd -- "$(dirname "${path_this_script}")" || return >/dev/null 2>&1
+    pwd -P
+  )"
+fi
 
 # help
 if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
@@ -36,27 +37,21 @@ if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
 fi
 
 # check number of arguments
-if (($# < NUM_POS_ARGS)) || (($# > (NUM_POS_ARGS + (NUM_OPT_ARGS * 2) + NUM_OPT_FLAGS))); then
+if (($# < NUM_POS_ARGS)) || (($# > (NUM_POS_ARGS + (NUM_OPT_ARGS * 2) + NUM_OPT_FLAGS + NUM_EXT_ARGS_MAX))); then
   echo "[error] invalid number of arguments"
-  print_help "${path_this_sript}"
+  print_help "${path_this_script}"
   exit 1
 fi
 
-# positional
-dir_downloads=${1%/}
-dir_apps=${2%/}
-dir_bin=${3%/}
-shift ${NUM_POS_ARGS}
-
-# optional - defaults
-download_deps=false
+# args optional - defaults
+download_pkgs=false
 helix_version='25.01.1'
 
-# optional
+# args optional
 while (($# > 0)); do
   case $1 in
-    -p | --download-deps)
-      download_deps=true
+    -p | --download-pkgs)
+      download_pkgs=true
       shift 1
       ;;
     -r | --version)
@@ -71,26 +66,45 @@ while (($# > 0)); do
   esac
 done
 
-# check dependencies
-for cmd in "${!DEPENDENCIES[@]}"; do
-  if [ "${cmd}" != '' ] && [ ! "$(command -v "${cmd}")" ] >/dev/null 2>&1; then
-    if ${download_deps}; then
-      echo "[info] dependency ${DEPENDENCIES["${cmd}"]} not found"
-      if [ "$EUID" -ne 0 ]; then
-        echo "[error] run this script in sudo to install missing deps"
-        exit 1
-      else
-        echo "[info] downloading ${DEPENDENCIES["${cmd}"]}"
-        sudo apt install "${DEPENDENCIES["${cmd}"]}" -y
-      fi
-    else
-      echo "[error] dependency ${DEPENDENCIES["${cmd}"]} not found"
-      exit 1
-    fi
-  fi
-done
+# args positional
+
+# relative path to absolute path
+# path_abs=$(readlink --canonicalize ${path})
+# path_abs=$(cd ${path}; pwd)
+dir_downloads=$(readlink --canonicalize "${1}")
+dir_apps=$(readlink --canonicalize "${2}")
+dir_bin=$(readlink --canonicalize "${3}")
+shift ${NUM_POS_ARGS}
+
+# args optional and extra
+if (($# > NUM_EXT_ARGS_MAX)); then
+  echo "[error] there are too many extra arguments"
+  exit 1
+else
+  declare -a args_extra=()
+  while (($# > 0)); do
+    args_extra+=("${1}")
+    shift 1
+  done
+fi
 
 # business
+# check directories
+if [ ! -d "${dir_downloads}" ]; then
+  echo "[error] ${dir_downloads} directory does not exist"
+  exit 1
+fi
+
+if [ ! -d "${dir_apps}" ]; then
+  echo "[error] ${dir_apps} directory does not exist"
+  exit 1
+fi
+
+if [ ! -d "${dir_bin}" ]; then
+  echo "[error] ${dir_bin} directory does not exist"
+  exit 1
+fi
+
 wget -nc -P "${dir_downloads}" "https://github.com/helix-editor/helix/releases/download/${helix_version}/helix-${helix_version}-x86_64-linux.tar.xz"
 tar -xvf "${dir_downloads}"/helix-"${helix_version}"-x86_64-linux.tar.xz --directory "${dir_apps}"/
 
@@ -101,3 +115,5 @@ fi
 
 rm -f "${dir_bin}"/hx
 ln -s "${dir_apps}"/helix-"${helix_version}"-x86_64-linux/hx "${dir_bin}"/hx
+
+echo "[info] success"
